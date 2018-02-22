538cdea4640c181a5825320f24e29ea1696fb554
apu <pasha_anik@ukr.net>
2018-02-22 15:53:01 +0200

    Add polling

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,22 @@
+var userId = null;
+var socket = null;
+
 function onloadfunction() {
 	var connect_but = document.getElementById("button_connect");
 	var new_rate_but = document.getElementById("button_new_rate");
 	connect_but.onclick = function() {	
 		
 			var lotId = document.getElementById("lot_id").value;
-			var userId = document.getElementById("user_id").value; 
+			userId = document.getElementById("user_id").value; 
 			if((lotId.length != 0) && (userId.length != 0)) {
 
 				helloMessage = "{\"observableLotIdList\":[" + lotId + "],\"queryType\":\"REGISTRATION\",\"time\":\"2017.12.08 13:01:18\",\"packetId\":0,\"userId\":" + userId + "}";
-				alert(helloMessage);
+				//alert(helloMessage);
 		
 				if("WebSocket" in window) {
 
 					var ws = new WebSocket("ws://localhost:5050/");
+					socket = ws;
 
 					ws.onopen = function() {
 						onOpenSocket(ws, helloMessage);
@@ -34,7 +38,7 @@ function onloadfunction() {
 					
 					new_rate_but.onclick = function() {
 						sendNewRate(ws, userId, lotId);
-					};
+					};					
 
 					connect_but.disabled = true;
 				} else {
@@ -48,6 +52,10 @@ function onloadfunction() {
 
 function onOpenSocket(socket, message) {		
 	socket.send(message);
+	setInterval(function() {
+		sendPollQuery(socket, userId);
+	}, 1000);
+	
 };
 
 function onReceiveMessage(message) {	
@@ -60,13 +68,18 @@ function sendNewRate(socket, userId, lotId) {
 	socket.send(message);
 }
 
-function parseMessage(message) {
-	alert(message);
+function sendPollQuery(socket, userId) {
+	var message = "{\"queryType\":\"POLL\",\"time\":\"2018.02.21 16:48:40.588\",\"packetId\":0,\"userId\":" + userId + "}";
+	socket.send(message);
+}
+
+function parseMessage(message) {	
 	var mess = JSON.parse(message);
 	if((mess.queryType == "ANSWER") && (mess.message == "OK")) {
 		//alert("receive OK message");
 		document.getElementById("time_last_update").value = mess.time;
 	} else if(mess.queryType == "NOTIFY") {
+		//alert(message);
 		var lotMess = mess.lot;
 		document.getElementById("start_price").value = lotMess.startPrice;
 		document.getElementById("current_rate").value = lotMess.lastRate;
@@ -74,6 +87,15 @@ function parseMessage(message) {
 		document.getElementById("winner_id").value = lotMess.lastRateUserId;
 		document.getElementById("observers").value = lotMess.amountObservers;
 		document.getElementById("time_to_finish").value = lotMess.timeToFinish;
+	} else if(mess.queryType == "POLL_ANSWER") {
+		//alert(message);
+		var lotMess = mess.auctionLots;
+		document.getElementById("start_price").value = lotMess[0].startPrice;
+		document.getElementById("lot_name").value = lotMess[0].lotName;
+		document.getElementById("current_rate").value = lotMess[0].lastRate;		
+		document.getElementById("winner_id").value = lotMess[0].lastRateUserId;
+		document.getElementById("observers").value = lotMess[0].amountObservers;
+		document.getElementById("time_to_finish").value = lotMess[0].timeToFinish;
 	}
 };
 
